a18f7411d86b3165d1221ecfbef9e1f747b34d82
Gustavo Beche Lopes <gustavo.beche@gmail.com>
2023-10-07 22:50:00 -0300

    adiciona parametro para iniciar com debug mode

--- cmd/clientTCP/main.go
+++ cmd/clientTCP/main.go
@@ -15,17 +15,24 @@ var (
 	MessagePort = flag.Int("message-port", 9000, "Port to send message")
 	CommandPort = flag.Int("command-port", 9001, "Port to send command")
 	User        = flag.String("user", "", "User to connect")
+	Debug       = flag.Bool("debug", false, "Debug mode")
 
 	Destination = flag.String("destination", "localhost", "Destination to send message")
 )
 
 func main() {
+	flag.Parse()
+
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
-	// zerolog.SetGlobalLevel(zerolog.DebugLevel)
-	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
-	flag.Parse()
+	if *Debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+		log.Debug().Msg("Debug mode enabled")
+	} else {
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	}
+
 	if *User == "" {
 		// Generate random user
 		*User = fmt.Sprintf("Anonymous_%d", rand.Intn(1000))
--- cmd/serverTCP/main.go
+++ cmd/serverTCP/main.go
@@ -13,17 +13,23 @@ var (
 	MessagePort = flag.Int("message-port", 9000, "Port to recive message")
 	CommandPort = flag.Int("command-port", 9001, "Port to recive command")
 	User        = flag.String("user", "", "User to connect")
+	Debug       = flag.Bool("debug", false, "Debug mode")
 
 	Listen = flag.String("listen", "127.0.0.1", "Listen to connect")
 )
 
 func main() {
+	flag.Parse()
+
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
-	// zerolog.SetGlobalLevel(zerolog.DebugLevel)
-	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
-	flag.Parse()
+	if *Debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+		log.Debug().Msg("Debug mode enabled")
+	} else {
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	}
 
 	serv := chat.ServerTCP{
 		Address:     *Listen,
